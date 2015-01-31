bae8be78a2c71106075e2db1c130426282294443
Jonathan <tetris24@gmail.com>
2015-01-31 15:16:35 -0500

    Update __init__.py

--- harvest/__init__.py
+++ harvest/__init__.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from metadata import (
-    __author__,
-    __copyright__,
-    __email__,
-    __license__,
-    __maintainer__,
-    __version__,
-)
-
 from harvest import *
 
 __all__ = [
-    '__author__', '__copyright__', '__email__', '__license__',
-    '__maintainer__', '__version__', 'harvest', 'status',
+    'harvest', 
+    'status',
 ]
